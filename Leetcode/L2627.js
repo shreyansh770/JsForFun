87fe20a368ed38c480f59b1278da3a9e384c80c7
shreyansh770 <shreyanshthakur1@gmail.com>
2024-03-21 22:56:33 +0530

    debouncing

---
+++ Leetcode/L2627.js
@@ -0,0 +1,13 @@
+var debounce = function(fn, t) {
+    let timer;
+    return function(...args) {
+        
+        if(timer){
+            clearTimeout(timer)
+        }
+
+        timer = setTimeout(()=>fn(...args),t)
+
+
+    }
+};
